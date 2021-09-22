057cf4e0b46131939b869f59954b97abdccd8374
DevTaehong <minth1123@icloud.com>
2021-09-22 03:17:20 -0300

    Module 1 Section2

---
+++ Labs/Lab1/Module1/Section2/M1S2/M1S2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1S2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the pass code?");
+            var code = Console.ReadLine();
+
+            if (code != "secret")
+            {
+                Console.WriteLine("Not Authenticated");
+            }
+
+            if (code == "secret")
+            {
+                Console.WriteLine("Authenticated");
+
+                Console.WriteLine("Change the pass code: ");
+                code = Console.ReadLine();
+                Console.WriteLine("New pass code is: {0}", code);
+
+            }
+
+            
+            
+            
+            
+            
+        }
+    }
+}
