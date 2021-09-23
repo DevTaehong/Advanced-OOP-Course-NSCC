035f27ffe756f249239524bfa63d3a408913ace9
DevTaehong <minth1123@icloud.com>
2021-09-22 21:33:46 -0300

    Module 1 Section 3 Done

---
+++ Labs/Lab1/Module1/Section3/M1S3/M1S3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1S3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int y = 1; y <= 5; y++){
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                    if (i == 10)
+                    {
+                        Console.WriteLine("10\n9\n8\n7\n6\n5\n4\n3\n2\n1");
+                    }
+                }
+            }
+
+                
+                
+
+           
+            
+
+        }
+    }
+}
