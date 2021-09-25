957d4011bcf4e027683029fe0bbbbf2d5782dcd3
DevTaehong <minth1123@icloud.com>
2021-09-25 16:58:51 -0300

    Module 3 SEction 1 done

---
+++ Labs/Lab1/Module3/Section1/M3S1/M3S1/Console.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Util
+{
+    class MyCustomException : FormatException
+    {
+        public MyCustomException(String msg) : base(msg)
+        {
+
+        }
+    }
+    class Console
+    {
+        
+        static public string Ask(string question)
+        {
+            System.Console.Write(question);
+            return System.Console.ReadLine();
+        }
+
+        static public int AskInt(string question)
+        {
+            try
+            {
+                System.Console.Write(question);
+                return int.Parse(System.Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                throw new MyCustomException("Input was not a number : From the custom one");
+            }
+        }
+    }
+}
